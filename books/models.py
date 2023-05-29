de6499b44997c9250310404be06f2231d38b6914
riograndeblood <dmitry.kondratiev4@gmail.com>
2023-05-29 21:48:52 +0300

    Добавлена модель жанров

--- books/models.py
+++ books/models.py
@@ -7,3 +7,6 @@ class Book(models.Model):
 
     def __str__(self):
         return f"книга:{self.id}, Название:{self.title}, автор:{self.author}"
+
+class Genre(models.Model):
+    title = models.CharField(max_length=50)
