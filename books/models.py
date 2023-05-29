9a3e107ac27a061049e9bb1fb5409ed744165575
riograndeblood <dmitry.kondratiev4@gmail.com>
2023-05-29 22:31:45 +0300

    Новые правки

--- books/models.py
+++ books/models.py
@@ -4,9 +4,14 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     year = models.IntegerField()
+    rating = models.IntegerField(default=0)
+    publisher = models.CharField(max_length=50, null=True)
+    genre = models.ForeignKey("Genre", on_delete=models.DO_NOTHING, null=True, blank=True, related_name="books")
 
     def __str__(self):
         return f"книга:{self.id}, Название:{self.title}, автор:{self.author}"
 
 class Genre(models.Model):
     title = models.CharField(max_length=50)
+    def __str__(self):
+        return f"Жанр: {self.id}, {self.title}"
